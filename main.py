32fcebfaad386de1ec9b5084eab02e43cf2e9c79
kholos, luciano <luciano.kho18@gmail.com>
2023-07-23 22:36:55 -0700

    Update main with mini-batch gradient descent

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from mnist_loader import *
 def test():
     X_train, Y_train, X_test, Y_test = get_mnist_data() # , X_test, Y_test
 
-    nn = NeuralNetwork([784, 100, 100, 10])
+    nn = NeuralNetwork([784, 100, 10])
 
     # Train on training data
-    nn.stochastic_gradient_descent(35, X_train, Y_train)
+    nn.mini_batch_gradient_descent(X_train, Y_train, 50, 10, X_test, Y_test)
 
     # Evaluate on test data
     nn.evaluate(X_test, Y_test)
